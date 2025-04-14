774ba11eca61ff819bc9ea83cd5611621e44e925
Shruti <dev.shruti27@gmail.com>
2025-04-14 18:00:10 +0200

    script file

---
+++ main.js
@@ -0,0 +1,9 @@
+"user-strict";
+
+const answer = document.getElementById("answer");
+const question = document.getElementById("question");
+const input = document.getElementsByClassName(".input");
+
+const searchBtn = document.getElementsByClassName(".btn__search");
+const newChatBtn = document.getElementsByClassName(".btn__new-chat");
+const sendBtn = document.getElementsByClassName(".btn__send");
